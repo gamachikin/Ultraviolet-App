446ab7ab8b5f9883c7f54b12d906f09bb9e96e36
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-08-10 13:11:23 -0400

    e

---
+++ index.js
@@ -0,0 +1,25 @@
+import { ChemicalServer } from "chemicaljs";
+import express from "express";
+import compression from "compression";
+
+
+const chemical = new ChemicalServer();
+const port = process.env.PORT || 8080;
+
+chemical.use(compression());
+
+chemical.use(
+  express.static("public", {
+    index: "proxy.html",
+    extensions: ["html"],
+  })
+);
+
+chemical.error((req, res) => {
+  res.status(404);
+  res.send("404 Error");
+});
+
+chemical.listen(port, () => {
+  console.log(`Games & Stuff Proxy listening on port ${port}`);
+});
